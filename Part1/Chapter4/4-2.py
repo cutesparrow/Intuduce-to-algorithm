a8a0d3d280d79acda9edd2ec18de71123b18f639
Ziven <shigaoyu615@gmail.com>
2020-12-30 15:18:37 +1100

    finish chapter2

--- Part1/Chapter4/4-2.py
+++ Part1/Chapter4/4-2.py
@@ -1,2 +1,22 @@
 #strassen
+import numpy
 
+a =     [[1,2,3],
+        [2,3,4],
+        [3,4,5]]
+
+b = [
+    [1,2,3],
+    [4,2,1],
+    [4,2,9]
+    ]
+def muti(a,b):
+    result = numpy.zeros((3,3),int)
+    for i in range(len(a)):
+        for j in range(len(b)):
+            for k in range(len(a)):
+                result[i][j] += a[i][k]*b[k][j]
+                print(result)
+    return result
+
+print(muti(a,b))
